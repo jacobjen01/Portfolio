f6ac751a6ce14017b36f5db1405c6c0328af5fce
jacobjen01 <jacobjen01@gmail.com>
2025-03-24 21:38:53 +0100

    s
    s

--- HireMeWebsite/script.js
+++
@@ -1,9 +0,0 @@
-document.getElementById("hireButton").addEventListener("click", function() {
-    let contactInfo = document.getElementById("contactInfo");
-    contactInfo.style.display = contactInfo.style.display === "none" ? "block" : "none";
-});
-
-document.getElementById("showImageButton").addEventListener("click", function() {
-    let image = document.getElementById("projectImage");
-    image.style.display = image.style.display === "none" ? "block" : "none";
-});
